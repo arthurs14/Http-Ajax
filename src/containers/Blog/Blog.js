67b3a66677862b30e4db0a9b0a843edabd1166a4
Arthur Sandro <arthurcs426@gmail.com>
2020-11-15 14:19:50 -0800

    instead of viewing over 100s of posts we narrowed it down to only 4 and made it possible to overwrite the author with a constant one

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,14 +14,23 @@ class Blog extends Component {
     componentDidMount() {
         // usually takes url as an argument
         axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-            this.setState({ posts: response.data })
+            const posts = response.data.slice(0, 4);
+            // can be used to add other values to the objects if needed
+            // one example though
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Arthur',
+                }
+            })
+            this.setState({ posts: updatedPosts })
             // console.log(response);
         });
     }
 
     render() {
         let posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} />
+            return <Post key={post.id} title={post.title} author={post.author} />
         });
 
         return (
