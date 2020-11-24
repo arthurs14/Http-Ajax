26845926546dd1c1e9926503b830677b67f7846d
Arthur Sandro <arthurcs426@gmail.com>
2020-11-24 15:52:01 -0800

    added withRouter to get the route parameters without having to transfer props from another component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
+// Helps functional components become route aware
+// import { withRouter } from 'react-router-dom';
+
 import './Post.css';
 
-const post = (props) => {
-    console.log(props);
-    return (
-        <article className="Post" onClick={props.clicked}>
-            <h1>{props.title}</h1>
-            <div className="Info">
-                <div className="Author">{props.author}</div>
-            </div>
-        </article>
-    );
-};
+const post = ({ clicked, title, author }) => (
+    <article className="Post" onClick={clicked}>
+        <h1>{title}</h1>
+        <div className="Info">
+            <div className="Author">{author}</div>
+        </div>
+    </article>
+);
 
 
 export default post;
