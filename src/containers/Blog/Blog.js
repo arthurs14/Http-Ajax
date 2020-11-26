999e617253850606528b93c6b1beacb2b45f7aa2
Arthur Sandro <arthurcs426@gmail.com>
2020-11-25 16:37:19 -0800

    used nested routes to make our urls rather than writing em in

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { Route, Link, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render() {
@@ -14,7 +13,7 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink
-                                to="/"
+                                to="/posts"
                                 exact
                                 activeClassName="my-active"
                                 activeStyle={{
@@ -33,9 +32,8 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" exact render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path="/" exact component={Posts} />
                     <Route path="/new-post" component={NewPost} />
-                    <Route path="/:id" exact component={FullPost} />
+                    <Route path="/posts" component={Posts} />
                 </Switch>
             </div>
         );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-//import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
+import FullPost from '../FullPost/FullPost';
 import './Posts.css';
 
 class Posts extends Component {
@@ -32,14 +33,14 @@ class Posts extends Component {
 
   postSelectedHandler = (id) => {
     //this.setState({ selectedPostId: id });
-    this.props.history.push({ pathname: `/${id}` });
+    this.props.history.push({ pathname: `/posts/${id}` });
   }
 
   render() {
     let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>
     posts = this.state.posts.map(post => {
       return (
-        // <Link key={post.id} to={`/${post.id}`}>
+        // <Link key={post.id} to={`/posts/${post.id}`}>
         //   <Post
         //     title={post.title}
         //     author={post.author}
@@ -54,9 +55,12 @@ class Posts extends Component {
     });
 
     return (
-      <section className="Posts">
-        {posts}
-      </section>
+      <div>
+        <section className="Posts">
+          {posts}
+        </section>
+        <Route path={`${this.props.match.url}/:id`} exact component={FullPost} />
+      </div>
     );
   }
 }
