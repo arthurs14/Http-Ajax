417fa8897672b563fd17dc68599e7c3958da70a3
Arthur Sandro <arthurcs426@gmail.com>
2020-11-25 16:24:23 -0800

    instead of updating the state, we can use the history to find the id to lead us to the full post

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -31,19 +31,25 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
+    //this.setState({ selectedPostId: id });
+    this.props.history.push({ pathname: `/${id}` });
   }
 
   render() {
     let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>
     posts = this.state.posts.map(post => {
       return (
-        <Link key={post.id} to={`/${post.id}`}>
-          <Post
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)} />
-        </Link>
+        // <Link key={post.id} to={`/${post.id}`}>
+        //   <Post
+        //     title={post.title}
+        //     author={post.author}
+        //     clicked={() => this.postSelectedHandler(post.id)} />
+        // </Link>
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)} />
       );
     });
 
